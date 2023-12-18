9c0c2cd9361bfbd14e960f2bc6b1708148093f30
Frédéric Lestage <frederic.lestage@gmail.com>
2023-12-17 21:57:10 -0500

    Added FLG.Cs.Framework missing files

---
+++ Cs/FLG.Cs.Framework/FrameworkManager.cs
@@ -0,0 +1,30 @@
+﻿using FLG.Cs.Decorators;
+using FLG.Cs.Factory;
+using FLG.Cs.Logger;
+using FLG.Cs.ServiceLocator;
+using FLG.Cs.UI;
+
+namespace FLG.Cs.Framework {
+    public class FrameworkManager : SingletonBase<FrameworkManager> {
+        private bool _initialized = false;
+        private FrameworkManager() { }
+
+        public void Initialize(Preferences p)
+        {
+            if (!_initialized)
+            {
+                _initialized = true;
+                LogManager.Instance.Initialize(p.logsDir);
+                ManagerFactory.CreateSerializer(p.serializerType, p.savesDir);
+                ManagerFactory.CreateUIManager(p.layoutsDir, p.pagesDir);
+            }
+        }
+
+        public void BootstrapUI()
+        {
+            // TODO: Make sure you have registered pages content before calling this method
+            // Locator.Instance.Get<IUIManager>().LoadUI();
+            // Observer pattenr to call the observers to then draw()
+        }
+    }
+}
---
+++ Cs/FLG.Cs.Framework/Preferences.cs
@@ -0,0 +1,11 @@
+﻿using FLG.Cs.Serialization;
+
+namespace FLG.Cs.Framework {
+    public struct Preferences {
+        public string logsDir;
+        public ESerializerType serializerType;
+        public string savesDir;
+        public string layoutsDir;
+        public string pagesDir;
+    }
+}
