3b104f558e5b25491b7178365e717fe24ee3d172
Frédéric Lestage <frederic.lestage@gmail.com>
2024-03-23 10:33:58 -0400

    Godot Logger

--- Cs/FLG.Cs.Logger/LogManager.cs
+++ Cs/FLG.Cs.Logger/LogManager.cs
@@ -5,18 +5,15 @@ using FLG.Cs.IDatamodel;
 
 namespace FLG.Cs.Logger {
     public class LogManager : ILogManager {
-        private const string FILENAME_DATE_PATTERN = @"yyyyddM_HH-mm";
-        private const string LOGGING_DATE_PATTERN = @"HH:mm:ss:fff";
-        private const string UNKNOWN = "Unknown";
-
         private string _logsDir;
         private string _filepath;
 
-        public LogManager(PreferencesLogs prefs) {
+        public LogManager(PreferencesLogs prefs)
+        {
             _logsDir = prefs.logsDir;
 
             DateTime date = DateTime.Now;
-            string filename = date.ToString(FILENAME_DATE_PATTERN);
+            string filename = date.ToString(LoggerMetadata.FILENAME_DATE_PATTERN);
             System.IO.Directory.CreateDirectory(_logsDir);
             _filepath = Path.Combine(_logsDir, filename + ".log");
         }
@@ -24,7 +21,8 @@ namespace FLG.Cs.Logger {
         #region IServiceInstance
         public bool IsProxy() => false;
         public void OnServiceRegisteredFail() { Error("Log Manager Failed to register"); }
-        public void OnServiceRegistered() {
+        public void OnServiceRegistered()
+        {
             Debug("Log Manager Registered");
         }
         #endregion IServiceInstance
@@ -37,7 +35,7 @@ namespace FLG.Cs.Logger {
 
             DateTime date = DateTime.Now;
             using StreamWriter w = File.AppendText(_filepath);
-            w.WriteLine($"[{date.ToString(LOGGING_DATE_PATTERN)}] [{level.ToPrettyString()}] [{(classname ?? UNKNOWN)}::{(methodname ?? UNKNOWN)}()] {msg}");
+            w.WriteLine($"[{date.ToString(LoggerMetadata.LOGGING_DATE_PATTERN)}] [{level.ToPrettyString()}] [{(classname ?? LoggerMetadata.UNKNOWN)}::{(methodname ?? LoggerMetadata.UNKNOWN)}()] {msg}");
         }
 
         public void Error(string msg)
