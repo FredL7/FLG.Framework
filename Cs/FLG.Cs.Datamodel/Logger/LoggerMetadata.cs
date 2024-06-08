b7c99c43f998dcab037d3769d256f6f5a63bc2e8
Frédéric Lestage <frederic.lestage@gmail.com>
2024-06-08 11:58:30 -0400

    Fix datetime format for log filename

--- Cs/FLG.Cs.Datamodel/Logger/LoggerMetadata.cs
+++ Cs/FLG.Cs.Datamodel/Logger/LoggerMetadata.cs
@@ -1,6 +1,6 @@
 ﻿namespace FLG.Cs.Datamodel {
     public static class LoggerMetadata {
-        public const string FILENAME_DATE_PATTERN = @"yyyyddM_HH-mm";
+        public const string FILENAME_DATE_PATTERN = @"yyyyMMdd_HH-mm";
         public const string LOGGING_DATE_PATTERN = @"HH:mm:ss:fff";
         public const string UNKNOWN = "Unknown";
     }
