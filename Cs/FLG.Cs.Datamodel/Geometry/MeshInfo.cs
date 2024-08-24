2ac6283068abd01083380cd187d4d46e87a725ab
Frédéric Lestage <frederic.lestage@gmail.com>
2024-08-24 12:39:21 -0400

    Added icosphere mesh generation

---
+++ Cs/FLG.Cs.Datamodel/Geometry/MeshInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Numerics;
+
+namespace FLG.Cs.Datamodel {
+    public struct MeshInfo {
+        public Vector3[] vertices;
+        public Vector3[] normals;
+        public Vector2[] uvs;
+        public int[] triangles;
+    }
+}
