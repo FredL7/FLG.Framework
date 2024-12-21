1f157a12cf274315213a9b6de2a2b3a728e06d25
Frédéric Lestage <frederic.lestage@gmail.com>
2024-12-21 12:09:20 -0500

    Creating a sample server executable

---
+++ Executables/ServerExecutable/Program.cs
@@ -0,0 +1,13 @@
+﻿using FLG.Cs.Datamodel.Framework;
+using FLG.Cs.Datamodel.Networking;
+using FLG.Cs.Framework;
+
+
+Console.Title = "Server";
+
+PreferencesFramework prefs = new()
+{
+    networking = new() { clientType = ENetworkingClientType.SERVER }
+};
+
+FrameworkManager.Instance.Initialize(prefs);
